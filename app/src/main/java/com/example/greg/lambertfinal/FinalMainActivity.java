7c4a5e1f51dd1557e5ea52133c188747b414c30f
Greg Lambert <lambertgreg19@gmail.com>
2017-01-02 16:57:59 -0500

    Update FinalMainActivity.java

--- app/src/main/java/com/example/greg/lambertfinal/FinalMainActivity.java
+++ app/src/main/java/com/example/greg/lambertfinal/FinalMainActivity.java
@@ -23,36 +23,40 @@ import android.widget.TextView;
 import android.widget.Toast;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteException;
-
 import com.google.android.gms.location.LocationListener;
-
-
 import org.json.JSONArray;
 import org.json.JSONObject;
-
 import java.io.File;
 import java.util.ArrayList;
 
-
-
-public class FinalMainActivity extends ActionBarActivity implements LocationListener {
+public class FinalMainActivity extends ActionBarActivity implements LocationListener { //creates an action bar, listens for gps location, sets objects required for project
 
     private LocationManager locationManager;
+    
     private String provider;
+    
     TextView latTxt;
+    
     TextView lngTxt;
+    
     ListView listView;
+    
     Double lat2;
+    
     Double lng2;
+    
     JSONObject jsonobject;
+    
     JSONArray jsonarray;
+   
     String placesSearchStr;
+    
     SQLiteDatabase db;
 
-    String[] types = {"any", "airport", "aquarium", "atm", "night_club", "police", "food", "hospital", "lawyer", "library", "subway_station", "school", "park", "pharmacy"};
-
-
+    String[] types = {"any", "airport", "aquarium", "atm", "night_club", "police", "food", "hospital", "lawyer", "library", "subway_station", "school", "park", "pharmacy"}; //default location types
+   
     ArrayList<Places> places;
+    
     ArrayList<String> names;
 
     ListView placeView;
@@ -64,11 +68,14 @@ public class FinalMainActivity extends ActionBarActivity implements LocationList
     Parcel in;
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {   //identify layout buttons with the strings we declared
+        
         super.onCreate(savedInstanceState);
+        
         setContentView(R.layout.activity_final_main);
+        
         latTxt = (TextView) findViewById(R.id.latTxt);
-        //lngTxt = (TextView) findViewById(R.id.lngTxt);
+      
         File storagePath = getApplication().getFilesDir();
 
         placeSpin = (Spinner) findViewById(R.id.placeSpin);
@@ -78,32 +85,43 @@ public class FinalMainActivity extends ActionBarActivity implements LocationList
         String myDbPath = storagePath + "/" + "favPlaces";
 
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, types);
+        
         placeSpin.setAdapter(adapter);
 
-        try {
+        try {                                                                   
 
-            db = SQLiteDatabase.openDatabase(myDbPath, null, SQLiteDatabase.CREATE_IF_NECESSARY);
+            db = SQLiteDatabase.openDatabase(myDbPath, null, SQLiteDatabase.CREATE_IF_NECESSARY);   //set up sqlite database
 
         } catch (SQLiteException e) {
 
 
         }
-
-
-        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE); //grab last known phone gps location
+        
         Criteria criteria = new Criteria();
+        
         provider = locationManager.getBestProvider(criteria, false);
+        
         Location location = locationManager.getLastKnownLocation(provider);
+        
         if (location != null) {
+            
             System.out.println("Provider " + provider + " has been selected.");
+            
             onLocationChanged(location);
-        } else {
+            
+        } 
+        
+        else {
             System.out.println("Location not available");
         }
 
 
-        placeSpin.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+        placeSpin.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() { //grab locations from google api, using the location coordinates we found, insert JSON data into a string we will parse through later
+            
             @Override
+            
             public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
                 if(placeSpin.getSelectedItem() == "any"){
 
@@ -111,56 +129,28 @@ public class FinalMainActivity extends ActionBarActivity implements LocationList
 
                     new DownloadJSON().execute();
 
-
-
-
-
                 }
 
                 else{
-
-
+                    
                     placesSearchStr = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=" + Double.toString(lat2) + "," + Double.toString(lng2) + "&types=" +placeSpin.getSelectedItem()+  "&radius=500&key=AIzaSyDtafYuCYHM0TqKhhdBnphl3xQe3a8488I";
+                    
                     new DownloadJSON().execute();
 
-
-
-
-
-
                 }
-
-
-                // your code here
             }
 
             @Override
+            
             public void onNothingSelected(AdapterView<?> parentView) {
-                // your code here
+            
             }
 
         });
 
-        //       Toast.makeText(getApplicationContext(), Double.toString(lng2), Toast.LENGTH_SHORT).show();
-
-//        placesSearchStr = "https://maps.googleapis.com/maps/api/place/nearbysearch/" +
-//                "json?location="+lat2+","+lng2+
-//                "&radius=1000&sensor=true" +
-//                "&types=food|bar|store|museum|art_gallery"+
-//                "&key=AIzaSyBfAF6-eEW3jiHwILIDK4pbCYBFKMH03K0";
-
-        // placesSearchStr = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=500&key=AIzaSyDtafYuCYHM0TqKhhdBnphl3xQe3a8488I";
-        //placesSearchStr = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=" + Double.toString(lat2) + "," + Double.toString(lng2) + "&radius=500&key=AIzaSyDtafYuCYHM0TqKhhdBnphl3xQe3a8488I";
-//&radius=500&types=food&name=cruise - rigth after coordinates and before &key
-        //new DownloadJSON().execute();
-
-
     }
 
-
-
-    private class DownloadJSON extends AsyncTask<Void, Void, Void> {
-
+    private class DownloadJSON extends AsyncTask<Void, Void, Void> { // JSON Parsing, grabs all the info we want from the json objects from placesSearchStr. We store this information in the places class found in Places.java. The places object is filled with the information we need and put into an arraylist.
 
         @Override
         protected Void doInBackground(Void... params) {
@@ -169,9 +159,7 @@ public class FinalMainActivity extends ActionBarActivity implements LocationList
 
             names = new ArrayList<String>();
 
-
             jsonobject = JSONfunctions.getJSONfromURL(placesSearchStr);
-
 
             try {
 
@@ -184,17 +172,15 @@ public class FinalMainActivity extends ActionBarActivity implements LocationList
 
                     JSONObject location = geometry.optJSONObject("location");
 
-
                     Places place = new Places();
 
                     place.setLat(location.optString("lat"));
+                    
                     place.setLng(location.optString("lng"));
 
                     if (geoJson.has("icon")) {
 
-                        place.setIconUrl(geoJson.optString("icon"));
-
-
+                        place.setIconUrl(geoJson.optString("icon"));    //grab google place icon associated with JSON object
 
                     }
                     else{
@@ -203,11 +189,7 @@ public class FinalMainActivity extends ActionBarActivity implements LocationList
 
                     }
 
-
-
-
                     String name = geoJson.optString("name");
-
 
                     place.setName(name);
 
@@ -221,18 +203,14 @@ public class FinalMainActivity extends ActionBarActivity implements LocationList
 
                     }
 
-
                     places.add(place);
-
-
+                    
                 }
-
 
             } catch (Exception e) {
 
 
             }
-
 
             return null;
 
@@ -242,14 +220,6 @@ public class FinalMainActivity extends ActionBarActivity implements LocationList
         @Override
         protected void onPostExecute(Void args) {
 
-            //int z = places.size();
-
-            //Places place2 = places.get(0);
-
-            //latTxt.setText(Integer.toString(z));
-
-            //lngTxt.setText(place2.getOperating());
-
             workTitle = new ArrayAdapter<String>(FinalMainActivity.this, android.R.layout.simple_list_item_checked, names);
 
             placeView.setAdapter(workTitle);
@@ -258,32 +228,22 @@ public class FinalMainActivity extends ActionBarActivity implements LocationList
 
                 Toast.makeText(getApplicationContext(), "Nothing Near You", Toast.LENGTH_LONG).show();
 
-
-
             }
-
-
+            
         }
 
     }
-
-
-
     /* Remove the locationlistener updates when Activity is paused */
 
     public void onLocationChanged(Location location) {
        Double lat = location.getLatitude();
        Double lng = location.getLongitude();
 
-        //lat2 = lat;
-        //lng2 = lng;
-
-        lat2 = -33.8670522;
+        lat2 = -33.8670522 //latlng test, 
         lng2 = 151.1957362;
-
-
-        //Toast.makeText(getApplicationContext(), lat + "----" + lng, Toast.LENGTH_LONG).show();
+        
         Log.i("", "Lattitude:" + lat);
+        
         Log.i("", "Longitude:" + lng);
     }
 
@@ -307,65 +267,61 @@ public class FinalMainActivity extends ActionBarActivity implements LocationList
 
 
     public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
-
-
+        
         super.onCreateContextMenu(menu, v, menuInfo);
+        
         populateMenu(menu);
+        
     }
 
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
+    public boolean onCreateOptionsMenu(Menu menu) { //add items to action bar
+                                                   // Inflate the menu;
         populateMenu(menu);
+        
         return super.onCreateOptionsMenu(menu);
-
-
+        
     }
 
-    public void populateMenu(Menu menu) {
+    public void populateMenu(Menu menu) { //adds items to action bar
+        
         int groupId = 0;
+        
         int order = 0;
 
         menu.add(groupId, 1, ++order, "Go to Selected in Maps");
+        
         menu.add(groupId, 2, ++order, "Add to Favorites");
+        
         menu.add(groupId, 3, ++order, "Go to Favorites");
 
-
     }
-
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         return applyMenuOption((item));
     }
 
-
-    public boolean applyMenuOption(MenuItem item) {
+    public boolean applyMenuOption(MenuItem item) { // listens for when an item in the action bar is clicked, details what will happen
         int itemid = item.getItemId();
-
 
         if (itemid == 1) {
 
-
             ArrayList<Places> bundleList = new ArrayList<Places>();
 
-
             int cntChoice = placeView.getCount();
-
-            //String checked = "";
-
-            //String unchecked = "";
 
             SparseBooleanArray sparseBooleanArray = placeView.getCheckedItemPositions();
 
             for (int i = 0; i < cntChoice; i++) {
 
                 if (sparseBooleanArray.get(i) == true) {
-                    //checked += placeView.getItemAtPosition(i).toString() + "\n";
+                    
                     bundleList.add(places.get(i));
+                    
                 } else if (sparseBooleanArray.get(i) == false) {
-                    //unchecked+= placeView.getItemAtPosition(i).toString() + "\n";
+                   //Do nothing
                 }
 
             }
@@ -374,9 +330,7 @@ public class FinalMainActivity extends ActionBarActivity implements LocationList
 
                 Toast.makeText(getApplicationContext(), "Please Select Place(s)", Toast.LENGTH_LONG).show();
 
-
             } else {
-
 
                 Intent abstractList = new Intent(FinalMainActivity.this, GoogleMap.class);
 
@@ -385,27 +339,22 @@ public class FinalMainActivity extends ActionBarActivity implements LocationList
                 myDataBundle.putParcelableArrayList("Places", bundleList);
 
                 myDataBundle.putDouble("myLat", lat2);
+                
                 myDataBundle.putDouble("myLng", lng2);
 
                 abstractList.putExtras(myDataBundle);
 
-                //Places place2 = bundleList.get(0);
-
-                //latTxt.setText(Double.toString(place2.getLng()));
-
                 startActivityForResult(abstractList, 101);
 
             }
-
-
+            
             return true;
         }
 
 
-        if (itemid == 2) {
+        if (itemid == 2) { //Put selected places into a favorites list. This opens up a SQlite database and populates it with all the information stored in our Places class
 
             ArrayList<Places> bundleList = new ArrayList<Places>();
-
 
             int cntChoice = placeView.getCount();
 
@@ -414,18 +363,17 @@ public class FinalMainActivity extends ActionBarActivity implements LocationList
             for (int i = 0; i < cntChoice; i++) {
 
                 if (sparseBooleanArray.get(i) == true) {
-                    //checked += placeView.getItemAtPosition(i).toString() + "\n";
+                    
                     bundleList.add(places.get(i));
+                    
                 } else if (sparseBooleanArray.get(i) == false) {
-                    //
+                    //Do nothing
                 }
             }
-
 
             if (bundleList.size() == 0) {
 
                 Toast.makeText(getApplicationContext(), "Please Select Place(s)", Toast.LENGTH_LONG).show();
-
 
             } else {
 
@@ -436,12 +384,10 @@ public class FinalMainActivity extends ActionBarActivity implements LocationList
                     String name = sqlPlace.getName();
 
                     String sqlLat = Double.toString(sqlPlace.getLat());
+                    
                     String sqlLng = Double.toString(sqlPlace.getLng());
+                    
                     String pic = sqlPlace.getIconUrl();
-
-                  //latTxt.setText(sqlLat);
-
-
 
                     String name2 = name;
 
@@ -449,11 +395,6 @@ public class FinalMainActivity extends ActionBarActivity implements LocationList
 
 
                     try {
-//                        db.execSQL("create table if not exists " + name + " ("
-//                                + " recID integer PRIMARY KEY autoincrement, "
-//                                + "name text, "
-//                                + "lat text, "
-//                                + "lng text);");
 
                         String sql = "CREATE TABLE IF NOT EXISTS " + name +
                                 "(ID INTEGER PRIMARY KEY autoincrement," +
@@ -462,61 +403,45 @@ public class FinalMainActivity extends ActionBarActivity implements LocationList
                                 " LNG       TEXT     NOT NULL, " +
                                 " PIC        TEXT     NOT NULL)";
 
-
-
                         db.execSQL(sql);
-
 
                     } catch (SQLiteException e) {
 
-
                         Toast.makeText(getApplicationContext(), "Error, Place in Unfreindly SQLLite Format", Toast.LENGTH_LONG).show();
-
 
                     }
 
-
                     try {
+                        
                         int id = 1;
 
                         db.execSQL("INSERT INTO " + name + " (NAME, LAT, LNG, PIC) VALUES ('"+name2+"','"+sqlLat+"', '"+sqlLng+"', '"+pic+"')");
-
 
                     } catch (SQLiteException e) {
 
                         Toast.makeText(getApplicationContext(), "Error, Unfreindly SQLLite Format", Toast.LENGTH_LONG).show();
 
-
                     }
-
-
-
-
                 }
-
-
             }
         }
 
-        if (itemid == 3) {
+        if (itemid == 3) { //Go to the favorites page, bundle up information we might need later and send it to the favorites page instance. Favorites.java
 
             Intent abstractList = new Intent(FinalMainActivity.this, favorites.class);
 
             Bundle myDataBundle = new Bundle();
 
-
             myDataBundle.putDouble("myLat", lat2);
+            
             myDataBundle.putDouble("myLng", lng2);
 
             abstractList.putExtras(myDataBundle);
-
-
-
+            
             startActivityForResult(abstractList, 102);
-
-
+            
         }
-
+        
         return true;
     }
 }
